59b6483fa53d9245fbf9f73eff53868014f5ff0e
Roman Burov <macmini@Mac-mini-Mac.local>
2024-10-23 01:21:28 +0300

    commit

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ window.addEventListener("scroll", function() {
     }
 });
 
-//ФОРМА ЗАЯВКИ
+//ФОРМА ЗАЯВКИ ТОВАРА
 
 document.addEventListener('DOMContentLoaded', function () {
-    let currentQuestion = 1;
-    const totalQuestions = 5;
+    let currentQuestionFirst = 1;
+    const totalQuestionsFirst = 5;
 
-    const questions = [
+    const questionsFirst = [
         { label: "Товар", placeholder: "Выберите товар" },
         { label: "Количество", placeholder: "Укажите количество товара" },
         { label: "Имя", placeholder: "Укажите свое имя" },
@@ -37,61 +37,53 @@ document.addEventListener('DOMContentLoaded', function () {
         { label: "Комментарий", placeholder: "Оставьте комментарий."}
     ];
 
-    const progressElement = document.getElementById('progress');
-    const questionCounter = document.getElementById('question-counter');
-    const formLabel = document.getElementById('form-label');
-    const inputField = document.getElementById('input-field');
-    const nextButton = document.getElementById('next-btn');
-    const prevButton = document.getElementById('prev-btn');
-    const auditForm = document.getElementById('audit-form');
-    const successMessage = document.getElementById('success-message');
-    const errorMessage = document.getElementById('error-message');
+    const progressElementFirst = document.getElementById('progress-first');
+    const questionCounterFirst = document.getElementById('question-counter-first');
+    const formLabelFirst = document.getElementById('form-label-first');
+    const inputFieldFirst = document.getElementById('input-field-first');
+    const nextButtonFirst = document.getElementById('next-btn-first');
+    const prevButtonFirst = document.getElementById('prev-btn-first');
+    const auditFormFirst = document.getElementById('audit-form-first');
+    const successMessageFirst = document.getElementById('success-message-first');
+    const errorMessageFirst = document.getElementById('error-message-first');
 
-    // Массив для хранения ответов на каждый вопрос
-    const answers = new Array(totalQuestions).fill('');
+    const answersFirst = new Array(totalQuestionsFirst).fill('');
 
     // Ваш токен и ID чата в Telegram
     const telegramToken = '7565359008:AAG-mPWEY8luvPOldIehVgE7QTHru4yDI10';
     const chatId = '6388010174';
 
-    // Функция получения текущей даты и времени в формате дд.мм.гггг и чч:мм
-    function getCurrentDateTime() {
+    function updateFormFirst() {
+        const progressPercentFirst = (currentQuestionFirst / totalQuestionsFirst) * 100;
+        progressElementFirst.style.width = `${progressPercentFirst}%`;
+        questionCounterFirst.textContent = `Вопрос ${currentQuestionFirst} / ${totalQuestionsFirst}`;
+
+        formLabelFirst.textContent = questionsFirst[currentQuestionFirst - 1].label;
+        inputFieldFirst.placeholder = questionsFirst[currentQuestionFirst - 1].placeholder;
+        inputFieldFirst.type = "text";
+        inputFieldFirst.value = answersFirst[currentQuestionFirst - 1] || '';
+        errorMessageFirst.style.display = 'none';
+        prevButtonFirst.style.display = currentQuestionFirst > 1 ? 'inline-block' : 'none';
+    }
+
+    function sendMessageToTelegramFirst() {
         const now = new Date();
         const day = String(now.getDate()).padStart(2, '0');
-        const month = String(now.getMonth() + 1).padStart(2, '0'); // Месяцы начинаются с 0
+        const month = String(now.getMonth() + 1).padStart(2, '0');
         const year = now.getFullYear();
         const hours = String(now.getHours()).padStart(2, '0');
         const minutes = String(now.getMinutes()).padStart(2, '0');
-        return `${day}.${month}.${year} ${hours}:${minutes}`;
-    }
+        const currentDate = `${day}.${month}.${year}`;
+        const currentTime = `${hours}:${minutes}`;
 
-    function updateForm() {
-        const progressPercent = (currentQuestion / totalQuestions) * 100;
-        progressElement.style.width = `${progressPercent}%`;
-        questionCounter.textContent = `Вопрос ${currentQuestion} / ${totalQuestions}`;
+        const message = `❗️Заявка по товару. Дата создания: ${currentDate}\n` +
+                        `Время создания: ${currentTime}\n\n` +
+                        `Товар: ${answersFirst[0]}\n` +
+                        `Количество: ${answersFirst[1]}\n` +
+                        `Имя: ${answersFirst[2]}\n` +
+                        `Телефон: ${answersFirst[3]}\n` +
+                        `Комментарий: ${answersFirst[4]}`;
 
-        // Обновляем поле формы
-        formLabel.textContent = questions[currentQuestion - 1].label;
-        inputField.placeholder = questions[currentQuestion - 1].placeholder;
-
-        // Если вопрос включает файл
-        if (questions[currentQuestion - 1].type === "file") {
-            inputField.type = "file";
-        } else {
-            inputField.type = "text";
-        }
-
-        // Восстанавливаем введенные данные для текущего вопроса
-        inputField.value = answers[currentQuestion - 1] || '';
-
-        // Скрываем сообщение об ошибке при переходе к новому вопросу
-        errorMessage.style.display = 'none';
-
-        // Показываем или скрываем кнопку "Назад" в зависимости от текущего вопроса
-        prevButton.style.display = currentQuestion > 1 ? 'inline-block' : 'none';
-    }
-
-    function sendMessageToTelegram(message) {
         const url = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
 
         fetch(url, {
@@ -113,61 +105,164 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 
-    function goToNextQuestion() {
-        // Сохраняем введенное значение в массив ответов
-        answers[currentQuestion - 1] = inputField.value.trim();
+    function goToNextQuestionFirst() {
+        answersFirst[currentQuestionFirst - 1] = inputFieldFirst.value.trim();
 
-        // Проверяем, что поле заполнено, если оно не является файлом или необязательным полем
-        if (questions[currentQuestion - 1].type !== "file" && inputField.value.trim() === '') {
-            errorMessage.textContent = 'Пожалуйста, заполните поле перед переходом к следующему вопросу.';
-            errorMessage.style.display = 'block';
+        if (inputFieldFirst.value.trim() === '') {
+            errorMessageFirst.textContent = 'Пожалуйста, заполните поле перед переходом к следующему вопросу.';
+            errorMessageFirst.style.display = 'block';
             return;
         }
 
-        // Если это последний вопрос, показать сообщение об успешной отправке и отправить данные в Telegram
-        if (currentQuestion === totalQuestions) {
-            auditForm.style.display = 'none'; // Скрыть форму
-            questionCounter.style.display = 'none'; // Скрыть счетчик вопросов
-            successMessage.style.display = 'block'; // Показать сообщение "Заявка отправлена"
+        if (currentQuestionFirst === totalQuestionsFirst) {
+            auditFormFirst.style.display = 'none';
+            questionCounterFirst.style.display = 'none';
+            successMessageFirst.style.display = 'block';
 
-            // Формируем сообщение для отправки в Telegram
-            const currentDateTime = getCurrentDateTime();
-            const message = `Заявка. Дата создания: ${currentDateTime.split(' ')[0]}\n` +
-                            `Время создания: ${currentDateTime.split(' ')[1]}\n\n` +
-                            `Товар: ${answers[0]}\n` +
-                            `Количество: ${answers[1]}\n` +
-                            `Имя: ${answers[2]}\n` +
-                            `Телефон: ${answers[3]}\n` +
-                            `Комментарий: ${answers[4]}`;
-
-            sendMessageToTelegram(message);
-
+            sendMessageToTelegramFirst();
         } else {
-            currentQuestion++;
-            updateForm();
+            currentQuestionFirst++;
+            updateFormFirst();
         }
     }
 
-    nextButton.addEventListener('click', goToNextQuestion);
-
-    prevButton.addEventListener('click', function () {
-        // Сохраняем введенное значение перед переходом на предыдущий вопрос
-        answers[currentQuestion - 1] = inputField.value.trim();
-
-        if (currentQuestion > 1) {
-            currentQuestion--;
-            updateForm();
+    nextButtonFirst.addEventListener('click', goToNextQuestionFirst);
+    prevButtonFirst.addEventListener('click', function () {
+        answersFirst[currentQuestionFirst - 1] = inputFieldFirst.value.trim();
+        if (currentQuestionFirst > 1) {
+            currentQuestionFirst--;
+            updateFormFirst();
         }
     });
 
-    // Добавляем прослушивание события "keydown" на поле ввода
-    inputField.addEventListener('keydown', function (event) {
+    inputFieldFirst.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
-            event.preventDefault(); // Чтобы форма не отправлялась по умолчанию
-            goToNextQuestion(); // Переход на следующий вопрос
+            event.preventDefault();
+            goToNextQuestionFirst();
         }
     });
 
-    // Инициализация формы
-    updateForm();
+    updateFormFirst();
+});
+
+//ФОРМА ЗАЯВКИ СОТРУДНИЧЕСТВА
+
+document.addEventListener('DOMContentLoaded', function () {
+    let currentQuestionSecond = 1;
+    const totalQuestionsSecond = 5;
+
+    const questionsSecond = [
+        { label: "Наименование компании", placeholder: "Введите наименование компании" },
+        { label: "Поставщик или дистрибьютор", placeholder: "Выберите поставщик или дистрибьютор" },
+        { label: "Имя", placeholder: "Введите ваше имя" },
+        { label: "Телефон", placeholder: "Введите ваш номер телефона" },
+        { label: "Комментарий", placeholder: "Оставьте комментарий" }
+    ];
+
+    const progressElementSecond = document.getElementById('progress-second');
+    const questionCounterSecond = document.getElementById('question-counter-second');
+    const formLabelSecond = document.getElementById('form-label-second');
+    const inputFieldSecond = document.getElementById('input-field-second');
+    const nextButtonSecond = document.getElementById('next-btn-second');
+    const prevButtonSecond = document.getElementById('prev-btn-second');
+    const auditFormSecond = document.getElementById('audit-form-second');
+    const successMessageSecond = document.getElementById('success-message-second');
+    const errorMessageSecond = document.getElementById('error-message-second');
+
+    const answersSecond = new Array(totalQuestionsSecond).fill('');
+
+    // Ваш токен и ID чата в Telegram
+    const telegramToken = '7565359008:AAG-mPWEY8luvPOldIehVgE7QTHru4yDI10';
+    const chatId = '6388010174';
+
+    function updateFormSecond() {
+        const progressPercentSecond = (currentQuestionSecond / totalQuestionsSecond) * 100;
+        progressElementSecond.style.width = `${progressPercentSecond}%`;
+        questionCounterSecond.textContent = `Вопрос ${currentQuestionSecond} / ${totalQuestionsSecond}`;
+
+        formLabelSecond.textContent = questionsSecond[currentQuestionSecond - 1].label;
+        inputFieldSecond.placeholder = questionsSecond[currentQuestionSecond - 1].placeholder;
+        inputFieldSecond.type = "text";
+        inputFieldSecond.value = answersSecond[currentQuestionSecond - 1] || '';
+        errorMessageSecond.style.display = 'none';
+        prevButtonSecond.style.display = currentQuestionSecond > 1 ? 'inline-block' : 'none';
+    }
+
+    function sendMessageToTelegramSecond() {
+        const now = new Date();
+        const day = String(now.getDate()).padStart(2, '0');
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const year = now.getFullYear();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const currentDate = `${day}.${month}.${year}`;
+        const currentTime = `${hours}:${minutes}`;
+
+        const message = `🤝 Заявка по сотрудничеству. Дата создания: ${currentDate}\n` +
+                        `Время создания: ${currentTime}\n\n` +
+                        `Наименование компании: ${answersSecond[0]}\n` +
+                        `Поставщик или дистрибьютор: ${answersSecond[1]}\n` +
+                        `Имя: ${answersSecond[2]}\n` +
+                        `Телефон: ${answersSecond[3]}\n` +
+                        `Комментарий: ${answersSecond[4]}`;
+
+        const url = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
+
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                chat_id: chatId,
+                text: message,
+            }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Message sent to Telegram:', data);
+        })
+        .catch(error => {
+            console.error('Error sending message to Telegram:', error);
+        });
+    }
+
+    function goToNextQuestionSecond() {
+        answersSecond[currentQuestionSecond - 1] = inputFieldSecond.value.trim();
+
+        if (inputFieldSecond.value.trim() === '') {
+            errorMessageSecond.textContent = 'Пожалуйста, заполните поле перед переходом к следующему вопросу.';
+            errorMessageSecond.style.display = 'block';
+            return;
+        }
+
+        if (currentQuestionSecond === totalQuestionsSecond) {
+            auditFormSecond.style.display = 'none';
+            questionCounterSecond.style.display = 'none';
+            successMessageSecond.style.display = 'block';
+
+            sendMessageToTelegramSecond();
+        } else {
+            currentQuestionSecond++;
+            updateFormSecond();
+        }
+    }
+
+    nextButtonSecond.addEventListener('click', goToNextQuestionSecond);
+    prevButtonSecond.addEventListener('click', function () {
+        answersSecond[currentQuestionSecond - 1] = inputFieldSecond.value.trim();
+        if (currentQuestionSecond > 1) {
+            currentQuestionSecond--;
+            updateFormSecond();
+        }
+    });
+
+    inputFieldSecond.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            goToNextQuestionSecond();
+        }
+    });
+
+    updateFormSecond();
 });
